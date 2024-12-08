910365966151fbce658aadf21aa3ecc741c8f873
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-12-09 01:09:28 +0600

    question Screen desgin start

--- lib/Screens/question_screen.dart
+++ lib/Screens/question_screen.dart
@@ -10,6 +10,29 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+                const Text("What is Flutter ?",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+        const SizedBox(height:20),
+
+        Container(
+
+          child: ElevatedButton(onPressed:(){}, child: Text('Option'),
+        ),
+
+        )
+      ],
+
+
+
+
+    );
   }
 }
